160ba024882deab04b6aa4e53ea259bbbb135958
caspater <mkapsata@gmail.com>
2023-04-13 10:21:14 +0200

    remove dead code

--- src/Services/LogPurgerService.php
+++ src/Services/LogPurgerService.php
@@ -42,44 +42,6 @@ class LogPurgerService
 
                 if ($hasNoRemainder) $policy->update(['last_purged_on' => today()]);
             });
-//        PurgingPolicy::ready()->each(function (PurgingPolicy $table) {
-//
-//            $date = DB::table($table->{'name'})
-//                ->whereDate('created_at', '<', today()->subDays($table->{'days_to_live'}))
-//                ->oldest()
-//                ->first(['created_at']);
-//
-//            if (is_null($date)) {
-//                $table->update(['last_run_on' => today()->toDateString()]);
-//                return;
-//            }
-//
-//            $date = Carbon::parse($date->created_at);
-//
-//            $records = DB::table($table->{'name'})
-//                ->whereDate('created_at', $date->toDateString())
-//                ->get();
-//
-//            $output = '';
-//
-//            foreach ($records as $record) {
-//                $output .= implode(',', get_object_vars($record));
-//                $output .= "\r\n";
-//            }
-//
-//            Storage::disk(config('purger.disk'))
-//                ->append(sprintf('%s%s%s.csv',
-//                    $table->{'name'},
-//                    DIRECTORY_SEPARATOR,
-//                    $date->format('Y_m_d')
-//                ), $output);
-//
-//            DB::table($table->{'name'})
-//                ->whereDate('created_at', $date->toDateString())
-//                ->delete();
-//
-//        });
-
         return 0;
     }
 
